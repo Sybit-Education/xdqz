efa271425145492fd8ef6376e1764e49651fda9e
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2022-04-19 20:43:49 +0200

    Add sass and extract bootstrap plugin

---
+++ src/plugins/bootstrap-vue.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+// Import custom SASS variable overrides, or alternatively
+// define your variable overrides in custom-vars.scss
+import '@/assets/scss/custom-vars.scss'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
