b8eab41cf76417e6dfe7bdbd35d792072c902413
Stephan Strittmatter <stephan.strittmatter@sybit.de>
2022-04-20 09:42:11 +0200

    add crono component

--- src/views/HighscoreView.vue
+++ src/views/HighscoreView.vue
@@ -1,11 +1,13 @@
 <template>
   <b-container class="highscore">
     <h1>Highscore</h1>
+    <hr>
     <highscore-item v-for="item in highscore" :key="item.id" :item="item"></highscore-item>
   </b-container>
 </template>
 
 <script>
+import { crono } from 'vue-crono'
 import highscoreService from '@/services/highscore.service'
 import HighscoreItem from '@/components/HighscoreItem.vue'
 
@@ -17,10 +19,20 @@ export default {
       highscore: []
     }
   },
+  mixins: [crono],
   mounted () {
-    highscoreService.getHighscore().then((result) => {
-      this.highscore = result
-    })
+    this.getData()
+  },
+  cron: {
+    time: 20 * 1000, // seconds * milliseconds
+    method: 'getData'
+  },
+  methods: {
+    getData () {
+      highscoreService.getHighscore().then((result) => {
+        this.highscore = result
+      })
+    }
   }
 
 }
